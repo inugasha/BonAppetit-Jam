34279c59c129f3001a3664ed835b32c46adf2a90
inugasha <pirsoncorentin@live.be>
2024-05-29 16:30:25 +0200

    Change patrol path gizmo

--- Assets/_/Features/Enemies/Patrol.cs
+++ Assets/_/Features/Enemies/Patrol.cs
@@ -1,9 +1,11 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Patrol : MonoBehaviour
 {
     [SerializeField] private Transform[] _patrolPoints;
     [SerializeField] private Color _patrolPathColor = Color.blue;
+    [SerializeField] private float _pathThickness = 3f;
 
     public Vector3 GetCurrentPatrolPointPosition() => _patrolPoints[_patrolPointIndex].position;
 
@@ -35,13 +37,23 @@ public class Patrol : MonoBehaviour
     {
         Gizmos.color = _patrolPathColor;
         if (_patrolPoints.Length < 2) return;
+        Vector3 point1;
+        Vector3 point2;
+
         for (int i = 0; i < _patrolPoints.Length - 1; i++)
         {
             if (_patrolPoints[i] == null || _patrolPoints[i + 1] == null) continue;
-            Gizmos.DrawLine(_patrolPoints[i].position, _patrolPoints[i + 1].position);
+            point1 = _patrolPoints[i].position;
+            point2 = _patrolPoints[i + 1].position;
+            Handles.DrawBezier(point1, point2, point1, point2, _patrolPathColor, null, _pathThickness);
         }
 
-        Gizmos.DrawLine(_patrolPoints[_patrolPoints.Length - 1].position, _patrolPoints[0].position);
+        point1 = _patrolPoints[_patrolPoints.Length - 1].position;
+        point2 = _patrolPoints[0].position;
+
+        Handles.DrawBezier(point1, point2, point1, point2, _patrolPathColor, null, _pathThickness);
+
+
     }
 
     private int _patrolPointIndex;
